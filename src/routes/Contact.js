02044fc9493878967ad14efe14409954a977d2a6
Jobwafula <jobwafulabg@gmail.com>
2023-04-30 00:22:01 +0300

    changes

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import Navbar from '../components/Navbar'
-import { Link } from 'react-router-dom'
 
 export default function Contact() {
   return (
